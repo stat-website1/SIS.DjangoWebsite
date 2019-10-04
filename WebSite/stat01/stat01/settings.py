e7e61f08f227319595e1466bacc3837e3e31c8ef
bessyhuang <bessy110576@gmail.com>
2019-10-04 14:00:10 +0800

    update (note: need to modify the dialogflow path in views.py)

--- WebSite/stat01/stat01/settings.py
+++ WebSite/stat01/stat01/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'stat01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_demo',
-        'USER': 'testchia_test',
-        'PASSWORD': 'testchia',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- WebSite/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ads/桌面/SIS.DjangoWebsite/WebSite/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
