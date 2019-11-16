d1f43fcc773402d6bafcbf0df0bed774b90a5515
ak4775989 <48211636+ak4775989@users.noreply.github.com>
2019-11-17 01:18:05 +0800

    website embellish

--- stat01/_sitemember/views.py
+++ stat01/_sitemember/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def sitemember_01(request):
     return render(request,'sitemember/01_sitemember.html',locals())
+def sitemember_02(request):
+    return render(request,'sitemember/02_sitemember.html',locals())
+def sitemember_03(request):
+    return render(request,'sitemember/03_sitemember.html',locals())
+def sitemember_04(request):
+    return render(request,'sitemember/04_sitemember.html',locals())
+def sitemember_05(request):
+    return render(request,'sitemember/05_sitemember.html',locals())
+def sitemember_06(request):
+    return render(request,'sitemember/06_sitemember.html',locals())
+def sitemember_07(request):
+    return render(request,'sitemember/07_sitemember.html',locals())
