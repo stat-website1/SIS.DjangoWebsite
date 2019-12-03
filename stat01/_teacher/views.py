57d2a8ae0fb32e9da7a63ef0ca4566d6c5ba43bf
ak4775989 <48211636+ak4775989@users.noreply.github.com>
2019-12-04 00:34:10 +0800

    increase teacher

--- stat01/_teacher/views.py
+++ stat01/_teacher/views.py
@@ -8,3 +8,39 @@ def teacher_02(request):
 
 def teacher_03(request):
     return render(request,"teacher/03_teacher.html",locals())
+
+def teacher_04(request):
+    return render(request,"teacher/04_teacher.html",locals())
+
+def teacher_05(request):
+    return render(request,"teacher/05_teacher.html",locals())
+
+def teacher_06(request):
+    return render(request,"teacher/06_teacher.html",locals())
+
+def teacher_07(request):
+    return render(request,"teacher/07_teacher.html",locals())
+
+def teacher_08(request):
+    return render(request,"teacher/08_teacher.html",locals())
+
+def teacher_09(request):
+    return render(request,"teacher/09_teacher.html",locals())
+
+def teacher_10(request):
+    return render(request,"teacher/10_teacher.html",locals())
+
+def teacher_11(request):
+    return render(request,"teacher/11_teacher.html",locals())
+
+def teacher_12(request):
+    return render(request,"teacher/12_teacher.html",locals())
+
+def teacher_13(request):
+    return render(request,"teacher/13_teacher.html",locals())
+
+def teacher_14(request):
+    return render(request,"teacher/14_teacher.html",locals())
+
+def teacher_15(request):
+    return render(request,"teacher/15_teacher.html",locals())
